8e74f3bf45a54e92818e26375edaa07460cc73d0
Jeff Liknes <jeff.liknes@gmail.com>
2018-11-01 11:33:48 -0700

    Board.js created

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,5 +1,5 @@
 import { SVG_NS } from "../settings";
-
+import Board from "./Board";
 
 
 export default class Game {
@@ -11,6 +11,7 @@ export default class Game {
 	
 		// Other code goes here...
 		this.gameElement = document.getElementById(this.element);
+		this.board = new Board(this.width, this.height);
 	}
 
 	render() {
@@ -21,6 +22,7 @@ export default class Game {
 		svg.setAttributeNS(null, 'height', this.height);
 		svg.setAttributeNS(null, 'viewbox', `0 0 ${this.width} ${this.height}`);
 		this.gameElement.appendChild(svg);
+		this.board.rendor(svg);
 		
 	}
 
